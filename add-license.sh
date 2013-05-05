15c9825fe42f4a3c5d01aa42c1748dd7f5dcd71e
shi <stefaan.himpe@gmail.com>
2013-05-05 22:39:09 +0200

    avoid tampering with autogenerated code

--- add-license.sh
+++ add-license.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-headache -h headache-conf/header.txt -c headache-conf/headache.conf `find | grep [\.]py$ | grep -v bitstring | grep -v pyeeg`
+headache -h headache-conf/header.txt -c headache-conf/headache.conf `find | grep [\.]py$ | grep -v bitstring | grep -v pyeeg | grep -v mainwindow`
